ae89ab9c785b0d9f5fbcc2117a3143ce7448be6a
Szajrii <michal05v@gmail.com>
2019-11-01 12:47:00 +0100

    register screen

---
+++ src/startpage/SigningControllers/SignIn.vue
@@ -0,0 +1,76 @@
+<template>
+
+    <v-app>
+        <div class="quizer-start">
+            <div class="quizer-signin">
+                <v-form>
+                    <v-text-field
+                            label="Email"
+                            outlined
+                            type="email"
+                            color="#5f99c9"
+                            :rules="[rules.emailMatch]"
+                    ></v-text-field>
+                    <v-text-field
+                            v-model="password"
+                            :append-icon="show1 ? 'fas fa-eye' : 'fas fa-eye-slash'"
+                            :rules="[rules.required, rules.min]"
+                            :type="show1 ? 'text' : 'password'"
+                            label="Password"
+                            counter
+                            color="#5f99c9"
+                            @click:append="show1 = !show1"
+                    ></v-text-field>
+                    <v-text-field
+                            label="Nickname"
+                            outlined
+                            color="#5f99c9"
+                    ></v-text-field>
+                </v-form>
+            </div>
+        </div>
+    </v-app>
+
+</template>
+
+<script>
+    export default {
+        name: "SignIn",
+
+        data () {
+            return {
+                show1: false,
+                password: '',
+                rules: {
+                    required: value => !!value || 'Required.',
+                    min: v => v.length >= 8 || 'Min 8 characters',
+                    emailMatch: v => this.validateEmail(v) || 'Invalid format'
+                },
+            }
+        },
+        methods: {
+            validateEmail(email) {
+                const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return reg.test(String(email).toLowerCase());
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .quizer-start {
+        width: 100%;
+        height: 100%;
+        background-image: url("../../img/start.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+    .quizer-signin {
+        width: 50%;
+        margin: auto;
+        padding-top: 10%;
+    }
+
+</style>
---
+++ src/startpage/StartPage.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-app>
+        <div class="quizer-start">
+            <div class="quizer-appinfo">
+                <li><span><i class="fas fa-medal" style="color: cornflowerblue"></i></span>Compete with other players</li>
+                <li><span><i class="fas fa-cog" style="color: lightgreen"></i></span>Create your own quiz</li>
+                <li><span><i class="far fa-lightbulb" style="color: yellow"></i></span>Learn something new</li>
+            </div>
+            <div class="quizer-sign">
+                <p> Do not wait! Join quizer community now! <span @click="navigateToRegistation">Sign up</span></p>
+                <p style="font-size: 1.6vw">If you don't have account</p>
+                <p><span>Sign in!</span></p>
+            </div>
+        </div>
+    </v-app>
+</template>
+
+<script>
+    import router from "../router"
+
+    export default {
+        name: "StartPage",
+
+        methods: {
+            navigateToRegistation() {
+                router.push({ path: '/Register' })
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .quizer-start {
+        width: 100%;
+        height: 100%;
+        background-image: url("../img/start.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+    .quizer-appinfo {
+        padding-top: 10%;
+        width: 70%;
+        margin: auto;
+        text-align: center;
+        font-family: Roboto;
+        color: white;
+        text-shadow: 2px 3px 2px #000000;
+        list-style-type: none;
+
+        li {
+            font-size: 2vw;
+            padding-top: 5px;
+        }
+        span {
+            padding-right: 12px;
+        }
+    }
+
+    .quizer-sign {
+        width: 70%;
+        margin: auto;
+        text-align: center;
+        font-family: Roboto;
+        color: white;
+        text-shadow: 2px 3px 2px #000000;
+        padding-top: 20px;
+        font-size: 2vw;
+
+        span {
+            color: deepskyblue;
+            cursor: pointer;
+        }
+        p {
+            padding-top: 10px;
+        }
+    }
+
+</style>
